af0af45bc3267e609d426e97dbf159348644dbe6
EmmanuelCastil4 <79658826+emmanuel084@users.noreply.github.com>
2021-09-25 20:06:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Write your code below this row 👇
+fizz = 0
+buzz = 0
+fizzbuzz = 0
+for number in range (1, 101):
+  
+  if number % 3:
+       
+    print("fizz")
+  elif number % 5:
+    
+    print("buzz")
+  elif number % 3 and number % 5:
+    
+    print("Fizzbuzz")
+ 
+  
+
+
